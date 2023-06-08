ed2cbd9c60d1ae30002ebfe464e43c1d503256c3
Dominik S <101213292+DominikSty@users.noreply.github.com>
2023-06-08 14:25:58 +0200

    udate model and create test

--- create_model.py
+++ create_model.py
@@ -1,38 +1,94 @@
 # Temat projektu: Projektowanie od podstaw architektury sieci do prostej klasyfikacji obrazów
 
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-# Definicja architektury sieci DNN
-def create_dnn_model():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),  # Warstwa wejściowa - spłaszczenie obrazu 28x28 pikseli
-        tf.keras.layers.Dense(256, activation='relu'),  # Warstwa ukryta z 256 neuronami i funkcją aktywacji ReLU
-        tf.keras.layers.Dense(128, activation='relu'),  # Dodatkowa warstwa ukryta z 128 neuronami i funkcją aktywacji ReLU
-        tf.keras.layers.Dense(64, activation='relu'),   # Dodatkowa warstwa ukryta z 64 neuronami i funkcją aktywacji ReLU
-        tf.keras.layers.Dense(10, activation='softmax')  # Warstwa wyjściowa z 10 neuronami (klasyfikacja na 10 klas) i funkcją aktywacji softmax
+
+def create_combined_model():
+    numClass = 10  # Liczba klas do sklasyfikowania
+
+    # Tworzenie modelu CNN
+    cnn_model = tf.keras.models.Sequential([
+        tf.keras.layers.Input(shape=(28, 28, 1)),
+        tf.keras.layers.Conv2D(20, (5, 5)),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Activation('relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(numClass, activation='softmax')
     ])
-    
+
+    # Tworzenie modelu DNN
+    dnn_model = tf.keras.models.Sequential([
+        tf.keras.layers.Reshape((numClass,), input_shape=(numClass,)),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(numClass, activation='softmax')
+    ])
+
+    # Tworzenie połączonego modelu
+    combined_model = tf.keras.models.Sequential([
+        tf.keras.layers.Input(shape=(28, 28, 1)),
+        cnn_model,
+        dnn_model
+    ])
+
     # Kompilacja modelu
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-    
-    return model
+    combined_model.compile(optimizer='adam',
+                           loss='sparse_categorical_crossentropy',
+                           metrics=['accuracy'])
+
+    return combined_model
+
 
 # Tworzenie instancji modelu
-model = create_dnn_model()
+model = create_combined_model()
 
 # Wczytanie danych treningowych i testowych
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
+# Podział danych treningowych na zbiór treningowy i walidacyjny
+val_split = 0.1
+val_samples = int(len(x_train) * val_split)
+x_val = x_train[:val_samples]
+y_val = y_train[:val_samples]
+x_train = x_train[val_samples:]
+y_train = y_train[val_samples:]
+
 # Przygotowanie danych treningowych
+x_train = x_train.reshape(-1, 28, 28)  # Usunięcie wymiaru dla kanału
 x_train = x_train / 255.0  # Normalizacja wartości pikseli do zakresu 0-1
 y_train = y_train.astype(int)
 
-# Trenowanie modelu
-model.fit(x_train, y_train, epochs=15)
+# Przygotowanie danych walidacyjnych
+x_val = x_val.reshape(-1, 28, 28)  # Usunięcie wymiaru dla kanału
+x_val = x_val / 255.0  # Normalizacja wartości pikseli do zakresu 0-1
+y_val = y_val.astype(int)
+
+# Trenowanie modelu z danymi walidacyjnymi
+history = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=15)
+
+# Wykresy procesu uczenia się
+plt.figure(figsize=(12, 4))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['loss'], label='Strata treningowa')
+plt.plot(history.history['val_loss'], label='Strata walidacyjna')
+plt.xlabel('Epoki')
+plt.ylabel('Strata')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['accuracy'], label='Dokładność treningowa')
+plt.plot(history.history['val_accuracy'], label='Dokładność walidacyjna')
+plt.xlabel('Epoki')
+plt.ylabel('Dokładność')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
 
 # Przygotowanie danych testowych
+x_test = x_test.reshape(-1, 28, 28)  # Usunięcie wymiaru dla kanału
 x_test = x_test / 255.0  # Normalizacja wartości pikseli do zakresu 0-1
 y_test = y_test.astype(int)
 
@@ -42,3 +98,96 @@ print('Dokładność modelu:', test_acc)
 
 # Zapis modelu do pliku
 model.save('model.h5')
+
+
+
+# import tensorflow as tf
+
+# # Definicja architektury sieci CNN
+# def create_cnn_model():
+#     numClass = 10  # Liczba klas do sklasyfikowania
+#     model = tf.keras.models.Sequential([
+#         tf.keras.layers.Input(shape=(28, 28, 1)),  # Warstwa wejściowa - obraz o rozmiarze 28x28 pikseli i 1 kanał
+#         tf.keras.layers.Conv2D(20, (5, 5)),  # Warstwa konwolucyjna z 20 filtrami o rozmiarze 5x5
+#         tf.keras.layers.BatchNormalization(),  # Warstwa normalizacji wsadowej
+#         tf.keras.layers.Activation('relu'),  # Warstwa aktywacji ReLU
+#         tf.keras.layers.Flatten(),  # Warstwa spłaszczająca
+#         tf.keras.layers.Dense(numClass, activation='softmax')  # Warstwa gęsta z funkcją aktywacji softmax
+#     ])
+
+#     # Kompilacja modelu
+#     model.compile(optimizer='adam',
+#                   loss='sparse_categorical_crossentropy',
+#                   metrics=['accuracy'])
+
+#     return model
+
+# # Tworzenie instancji modelu
+# model = create_cnn_model()
+
+# # Wczytanie danych treningowych i testowych
+# (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# # Przygotowanie danych treningowych
+# x_train = x_train.reshape(-1, 28, 28, 1)  # Dodanie wymiaru dla kanału
+# x_train = x_train / 255.0  # Normalizacja wartości pikseli do zakresu 0-1
+# y_train = y_train.astype(int)
+
+# # Trenowanie modelu
+# model.fit(x_train, y_train, epochs=15)
+
+# # Przygotowanie danych testowych
+# x_test = x_test.reshape(-1, 28, 28, 1)  # Dodanie wymiaru dla kanału
+# x_test = x_test / 255.0  # Normalizacja wartości pikseli do zakresu 0-1
+# y_test = y_test.astype(int)
+
+# # Ocena dokładności modelu na danych testowych
+# test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+# print('Dokładność modelu:', test_acc)
+
+# # Zapis modelu do pliku
+# model.save('model.h5')
+
+
+# import tensorflow as tf
+
+# # Definicja architektury sieci DNN
+# def create_dnn_model():
+#     model = tf.keras.models.Sequential([
+#         tf.keras.layers.Flatten(input_shape=(28, 28)),  # Warstwa wejściowa - spłaszczenie obrazu 28x28 pikseli
+#         tf.keras.layers.Dense(256, activation='relu'),  # Warstwa ukryta z 256 neuronami i funkcją aktywacji ReLU
+#         tf.keras.layers.Dense(128, activation='relu'),  # Dodatkowa warstwa ukryta z 128 neuronami i funkcją aktywacji ReLU
+#         tf.keras.layers.Dense(64, activation='relu'),   # Dodatkowa warstwa ukryta z 64 neuronami i funkcją aktywacji ReLU
+#         tf.keras.layers.Dense(10, activation='softmax')  # Warstwa wyjściowa z 10 neuronami (klasyfikacja na 10 klas) i funkcją aktywacji softmax
+#     ])
+    
+#     # Kompilacja modelu
+#     model.compile(optimizer='adam',
+#                   loss='sparse_categorical_crossentropy',
+#                   metrics=['accuracy'])
+    
+#     return model
+
+# # Tworzenie instancji modelu
+# model = create_dnn_model()
+
+# # Wczytanie danych treningowych i testowych
+# (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# # Przygotowanie danych treningowych
+# x_train = x_train / 255.0  # Normalizacja wartości pikseli do zakresu 0-1
+# y_train = y_train.astype(int)
+
+# # Trenowanie modelu
+# model.fit(x_train, y_train, epochs=15)
+
+# # Przygotowanie danych testowych
+# x_test = x_test / 255.0  # Normalizacja wartości pikseli do zakresu 0-1
+# y_test = y_test.astype(int)
+
+# # Ocena dokładności modelu na danych testowych
+# test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+# print('Dokładność modelu:', test_acc)
+
+# # Zapis modelu do pliku
+# model.save('model.h5')
--- test_model.py
+++ test_model.py
@@ -1,28 +1,81 @@
+import tkinter as tk
+from PIL import Image, ImageDraw
 import tensorflow as tf
 import numpy as np
-from PIL import Image
+import os
 
-def load_model_from_file(file_path):
-    model = tf.keras.models.load_model(file_path)
-    return model
+# Rozmiar pojedynczego piksela
+PIXEL_SIZE = 10
+# Rozmiar planszy (w pikselach)
+BOARD_SIZE = 28
 
-def preprocess_image(image_path):
-    image = Image.open(image_path).convert('L')  # Konwersja obrazu do odcieni szarości
-    image = image.resize((28, 28))  # Zmiana rozmiaru obrazu na 28x28 pikseli
-    image = np.array(image) / 255.0  # Normalizacja wartości pikseli do zakresu 0-1
-    image = np.expand_dims(image, axis=0)  # Dodanie dodatkowego wymiaru dla wsadu (batch)
-    return image
+class DrawingApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Test modelu")
+        window_width = 600
+        window_height = 400
+        # Ustaw szerokość i wysokość okna
+        self.root.geometry(f"{window_width}x{window_height}")
 
-# Wczytanie modelu z pliku
-loaded_model = load_model_from_file("model.h5")
+        self.canvas = tk.Canvas(self.root, width=BOARD_SIZE * PIXEL_SIZE, height=BOARD_SIZE * PIXEL_SIZE, bg='black')
+        self.canvas.pack()
+        self.canvas.place(x=20, y=100)
+        self.canvas.bind('<B1-Motion>', self.draw_pixel)
 
-# Przygotowanie obrazu testowego
-image_path = "image_for_test/image_0.jpg"
-preprocessed_image = preprocess_image(image_path)
+        self.button_save = tk.Button(self.root, text='Sprawdź', command=self.save_image)
+        self.button_save.place(x=340, y=120)
 
-# Klasyfikacja obrazu
-predictions = loaded_model.predict(preprocessed_image)
-predicted_class = np.argmax(predictions)
+        self.clear_button = tk.Button(self.root, text="Wyczyść", command=self.clear_canvas)
+        self.clear_button.place(x=340, y=160)
 
-# Wyświetlenie wyników
-print("Przewidziana klasa:", predicted_class)
+        self.result_label = tk.Label(self.root, text="", fg="black", font=("Arial", 12))
+        self.result_label.place(x=340, y=260)
+
+        self.info_label1 = tk.Label(self.root, text="Program przeznaczony do testowania modelu sieci do rozpoznawania obrazów", fg="black", font=("Arial", 11))
+        self.info_label1.place(x=10, y=10)
+        self.info_label2 = tk.Label(self.root, text="liczb, czarno-biały obraz w rozdzielczości 28x28 pikseli poddawany jest predykcji", fg="black", font=("Arial", 11))
+        self.info_label2.place(x=10, y=30)
+        self.info_label3 = tk.Label(self.root, text="na wyuczonym modelu i zwraca przewidywaną wartość. Poniżej znaduje się płutno, ", fg="black", font=("Arial", 11))
+        self.info_label3.place(x=10, y=50)
+        self.info_label4 = tk.Label(self.root, text="na którym PPM można narysować własną liczbę do sprawdzenia.", fg="black", font=("Arial", 11))
+        self.info_label4.place(x=10, y=70)
+
+        self.image = Image.new('RGB', (BOARD_SIZE, BOARD_SIZE), 'black')
+        self.draw = ImageDraw.Draw(self.image)
+
+    def draw_pixel(self, event):
+        x = event.x // PIXEL_SIZE
+        y = event.y // PIXEL_SIZE
+        self.draw.point((x, y), fill='white')
+        self.canvas.create_rectangle(x * PIXEL_SIZE, y * PIXEL_SIZE,
+                                     (x + 1) * PIXEL_SIZE, (y + 1) * PIXEL_SIZE,
+                                     fill='white')
+        
+    def clear_canvas(self):
+        self.canvas.delete("all")
+        self.image = Image.new('RGB', (BOARD_SIZE, BOARD_SIZE), 'black')
+        self.draw = ImageDraw.Draw(self.image)
+        self.result_label.config(text="")
+
+    def save_image(self):
+        file_path = "buffor"
+        self.image.save(file_path, "JPEG")
+
+        self.model = tf.keras.models.load_model("model.h5")
+        image_a = Image.open(file_path).convert('L')        # Konwersja obrazu do odcieni szarości
+        image_a = image_a.resize((28, 28))               # Zmiana rozmiaru obrazu na 28x28 pikseli
+        image_a = np.array(image_a) / 255.0              # Normalizacja wartości pikseli do zakresu 0-1
+        image_a = np.expand_dims(image_a, axis=0)        # Dodanie dodatkowego wymiaru dla wsadu (batch)
+        predictions = self.model.predict(image_a)
+        predicted_class = np.argmax(predictions)
+
+        print("Wynik:", predicted_class)
+        self.result_label.config(text="Przewidywana liczba: " + str(predicted_class))
+
+        os.remove(file_path)
+
+
+root = tk.Tk()
+app = DrawingApp(root)
+root.mainloop()

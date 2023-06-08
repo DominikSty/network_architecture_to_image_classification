411645ed886aa98d0fe32114702d2093048feb98
Dominik S <101213292+DominikSty@users.noreply.github.com>
2023-06-08 16:18:27 +0200

    Update files

--- src/test_model.py
+++ src/test_model.py
@@ -18,7 +18,10 @@ class DrawingApp:
         # Ustaw szerokość i wysokość okna
         self.root.geometry(f"{window_width}x{window_height}")
 
-        self.canvas = tk.Canvas(self.root, width=BOARD_SIZE * PIXEL_SIZE, height=BOARD_SIZE * PIXEL_SIZE, bg='black')
+        self.canvas = tk.Canvas(self.root, 
+                                width=BOARD_SIZE * PIXEL_SIZE, 
+                                height=BOARD_SIZE * PIXEL_SIZE, 
+                                bg='black')
         self.canvas.pack()
         self.canvas.place(x=20, y=100)
         self.canvas.bind('<B1-Motion>', self.draw_pixel)
@@ -32,13 +35,21 @@ class DrawingApp:
         self.result_label = tk.Label(self.root, text="", fg="black", font=("Arial", 12))
         self.result_label.place(x=340, y=260)
 
-        self.info_label1 = tk.Label(self.root, text="Program przeznaczony do testowania modelu sieci do rozpoznawania obrazów", fg="black", font=("Arial", 11))
+        self.info_label1 = tk.Label(self.root, 
+                                    text="Program przeznaczony do testowania modelu sieci do rozpoznawania obrazów", 
+                                    fg="black", font=("Arial", 11))
         self.info_label1.place(x=10, y=10)
-        self.info_label2 = tk.Label(self.root, text="liczb, czarno-biały obraz w rozdzielczości 28x28 pikseli poddawany jest predykcji", fg="black", font=("Arial", 11))
+        self.info_label2 = tk.Label(self.root, 
+                                    text="liczb, czarno-biały obraz w rozdzielczości 28x28 pikseli poddawany jest predykcji", 
+                                    fg="black", font=("Arial", 11))
         self.info_label2.place(x=10, y=30)
-        self.info_label3 = tk.Label(self.root, text="na wyuczonym modelu i zwraca przewidywaną wartość. Poniżej znaduje się płutno, ", fg="black", font=("Arial", 11))
+        self.info_label3 = tk.Label(self.root, 
+                                    text="na wyuczonym modelu i zwraca przewidywaną wartość. Poniżej znaduje się płutno, ", 
+                                    fg="black", font=("Arial", 11))
         self.info_label3.place(x=10, y=50)
-        self.info_label4 = tk.Label(self.root, text="na którym PPM można narysować własną liczbę do sprawdzenia.", fg="black", font=("Arial", 11))
+        self.info_label4 = tk.Label(self.root, 
+                                    text="na którym PPM można narysować własną liczbę do sprawdzenia.", 
+                                    fg="black", font=("Arial", 11))
         self.info_label4.place(x=10, y=70)
 
         self.image = Image.new('RGB', (BOARD_SIZE, BOARD_SIZE), 'black')
@@ -61,18 +72,16 @@ class DrawingApp:
     def save_image(self):
         file_path = "buffor"
         self.image.save(file_path, "JPEG")
-
+        # Test modelu
         self.model = tf.keras.models.load_model("src/model/model.h5")
-        image_a = Image.open(file_path).convert('L')        # Konwersja obrazu do odcieni szarości
-        image_a = image_a.resize((28, 28))               # Zmiana rozmiaru obrazu na 28x28 pikseli
-        image_a = np.array(image_a) / 255.0              # Normalizacja wartości pikseli do zakresu 0-1
-        image_a = np.expand_dims(image_a, axis=0)        # Dodanie dodatkowego wymiaru dla wsadu (batch)
+        image_a = Image.open(file_path).convert('L')  # Konwersja obrazu do odcieni szarości
+        image_a = image_a.resize((28, 28))            # Zmiana rozmiaru obrazu na 28x28 pikseli
+        image_a = np.array(image_a) / 255.0           # Normalizacja wartości pikseli do zakresu 0-1
+        image_a = np.expand_dims(image_a, axis=0)     # Dodanie dodatkowego wymiaru dla wsadu (batch)
         predictions = self.model.predict(image_a)
         predicted_class = np.argmax(predictions)
-
         print("Wynik:", predicted_class)
         self.result_label.config(text="Przewidywana liczba: " + str(predicted_class))
-
         os.remove(file_path)
 
 
